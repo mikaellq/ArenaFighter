3c650c8d43049ad637b0780965651c7217318a80
mikaellq <38102084+mikaellq@users.noreply.github.com>
2018-04-20 14:50:45 +0200

    Initial commit

---
+++ ConsoleApp2/GameData.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaFighter
+{
+    // Character is used for both player and opponent
+    class Character
+    {
+        string name;
+        int sum;
+        int role;
+
+        public Character()
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            sum = r.Next(5, 15);
+            role = 0;
+        }
+
+        public string ReturnString()
+        {
+            string s = name + ": Role: " + role + ", Sum: " + sum;
+            return s;
+        }
+
+        public void SetRoleSum(int d, int p)
+        {
+            role = d;
+            sum += p;
+        }
+
+        public int GetSum() { return sum; }
+        public string GetName() { return name; }
+        public void SetName(string newName) { name = newName; }
+    }
+
+    // Usage of inheritance that I thought is elegant only adding one functionality for the opponent and using the rest already defined in Character
+    class EnemyCharacter : Character
+    {
+        public EnemyCharacter()
+        {
+            string[] names = new string[] { "Daniel", "Linus", "Jonathan", "Isaac", "Jason", "Jens", "Amir", "Chaminda", "Krall" };
+            Random r = new Random(DateTime.Now.Millisecond);
+            int i = names.Length;
+            SetName(names[r.Next(i)]);
+        }
+    }
+
+    class Battle
+    {
+        List<string> log = new List<string>();
+
+        public void AddToLog(Character p, Character e)
+        {
+            string s = p.ReturnString() + " -- " + e.ReturnString();
+            log.Add(s);
+            //            string t = string.Join(", \n", log.ToArray());
+            Console.Write(s);
+        }
+    }
+
+    class Round
+    {
+        public Round(Character p, Character e, Battle b)
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            int dicep = r.Next(1, 7);
+            int dicee = r.Next(1, 7);
+            if (dicep != dicee) // To avoid endless games
+            {
+                int punishp = dicep, punishe = dicee;
+
+                // Deciding who to punish and how much and also trying to create short code for it
+                if (dicep > dicee) { punishe = -dicep; }
+                else if (dicee > dicep) { punishp = -dicee; }
+
+                p.SetRoleSum(dicep, punishp);
+                e.SetRoleSum(dicee, punishe);
+            }
+            b.AddToLog(p, e);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaFighter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oldName = "Mikael";
+            do
+            {
+                Console.Write("Your name please, only Enter if you want to keep previous name: ");
+                string myName = Console.ReadLine();
+                string myName2 = myName.Trim();
+                Character player = new Character();
+                if (myName2.Length > 0) {
+                    player.SetName(myName2);
+                    oldName = myName2;
+                }
+                else
+                {
+                    player.SetName(oldName);
+                }
+
+                EnemyCharacter opponent = new EnemyCharacter();
+                Battle battle = new Battle();
+                char pressedKey = '\r';
+                do
+                {
+                    Round round = new Round(player, opponent, battle);
+
+                    if (player.GetSum() > 0 && opponent.GetSum() > 0)
+                    {
+                        Console.Write(" \t");
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Press 'q' to exit!");
+                        Console.ResetColor();
+                        pressedKey = Console.ReadKey().KeyChar;
+                    }
+                } while (pressedKey != 'q' && player.GetSum() > 0 && opponent.GetSum() > 0);
+                string s;
+                if (player.GetSum() < opponent.GetSum()) { s = opponent.GetName(); }
+                else { s = player.GetName(); }
+                Console.Beep(600, 200);
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n -"+s + " won!- ");
+                Console.ResetColor();
+//                Console.WriteLine("-------------------------------------");
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Play again? Press Enter in that case!");
+                Console.ResetColor();
+//                Console.WriteLine("-------------------------------------\n");
+            } while (Console.ReadKey().KeyChar == '\r');
+        }
+    }
+}
